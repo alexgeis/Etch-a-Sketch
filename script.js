10e23f2a549a5c4337d4c85b9ffb1e826084dd07
Alex Geis <siegxela@gmail.com>
2022-05-16 15:24:27 -0600

    update event listeners for button active class

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import { hasClass, addClass, removeClass } from "./assets/js/helpers";
-
 const colorPicker = document.querySelector("#colorPicker");
 const buttons = document.querySelectorAll("button");
 const btnColor = document.querySelector("#btnColor");
@@ -9,13 +7,68 @@ const btnClear = document.querySelector("#btnClear");
 const gridSlider = document.querySelector("#gridSlider");
 const circle1 = document.querySelector("#circle1");
 const circle2 = document.querySelector("#circle2");
+const currentYear = document.querySelector("#currentYear");
 
-// document.body.addEventListener("click", print);
+currentYear.textContent = document.write(new Date().getFullYear());
 
+// HELPER FUNCTIONS
+function hasClass(el, className) {
+  return el.classList
+    ? el.classList.contains(className)
+    : new RegExp("\\b" + className + "\\b").test(el.className);
+}
+
+function addClass(el, className) {
+  if (el.classList) el.classList.add(className);
+  else if (!hasClass(el, className)) el.className += " " + className;
+}
+
+function removeClass(el, className) {
+  if (el.classList) el.classList.remove(className);
+  else
+    el.className = el.className.replace(
+      new RegExp("\\b" + className + "\\b", "g"),
+      ""
+    );
+
+  /*
+   REGEX EXPLAINED
+   \ = reg ex boundaries
+   \b = word boundaries
+   g = global search
+*/
+}
+
+document.body.addEventListener("click", print);
 function print(e) {
   console.log(e);
 }
 
-buttons.addEventListener("click", activeButton);
+//add event listeners to all buttons
+for (let button of Array.from(buttons)) {
+  button.addEventListener("click", updateActiveButton);
+}
 
-function activeButton(e) {}
+function updateActiveButton(e) {
+  if (e.target.id === "buttonClear") {
+    removeActive();
+    // clearGrid();
+  } else {
+    removeActive();
+    activeButton(e);
+  }
+}
+
+function removeActive() {
+  for (let button of Array.from(buttons)) {
+    if (hasClass(button, "active")) {
+      removeClass(button, "active");
+    }
+  }
+}
+
+function activeButton(e) {
+  e.preventDefault();
+  console.log(e);
+  addClass(e.target, "active");
+}
